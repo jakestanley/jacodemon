2c54fbf81a702664ba58f4f900c1726852cad2c7
Jake Stanley <prudentia713@gmail.com>
2023-08-20 17:44:22 +0100

    Notifications: silence dismissal timeouts

--- lib/py/platform/windows/notifications.py
+++ lib/py/platform/windows/notifications.py
@@ -1,9 +1,9 @@
 from lib.py.notifications import Notifications
-from win11toast import toast
+from win11toast import toast, notify
 
 class WinNotifications(Notifications):
     def __init__(self):
         super().__init__()
 
     def notify(self, title, body):
-        toast(title, body)
+        notify(title, body)
